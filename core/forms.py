63dbec9bfb8c0388a4c3b2131a9888a04a15d159
lenivaldo <llneivaldo177@gmail.com>
2023-01-27 15:39:10 -0300

    Back dos cruds

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,27 @@
 from django.forms import ModelForm
-from .models import Espaco
+from .models import Espaco, Atividade, Nivel, Status, Modalidades
 
 class EspacosForm(ModelForm):
     class Meta:
         model = Espaco
         fields = ['nome_espaco']
+
+class AtividadeForm(ModelForm):
+    class Meta:
+        model =  Atividade
+        fields = ['data_hora_inicio', 'data_hora_fim', 'responsavel', 'quantidade', 'turma' ]
+
+class NivelForm(ModelForm):
+    class Meta:
+        model = Nivel
+        fields = ['Nivel']
+
+class StatusForm(ModelForm):
+    class Meta:
+        model = Status
+        fields = ['Status']
+
+class ModalidadeForm(ModelForm):
+    class Meta:
+        model = Modalidades
+        fields = ['NomeModalidade']
